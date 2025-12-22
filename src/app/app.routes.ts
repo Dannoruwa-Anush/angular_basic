37c617bb5ac4e5b6220c3219005274cbe1ff5c64
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2025-12-22 09:44:58 +0530

    basic routes are added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/reusable_components/layout-component/layout-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        //root route
+        path: '', component: LayoutComponent,
+
+        //nested route
+        children: [
+            // TODO
+        ]
+    }
+];
